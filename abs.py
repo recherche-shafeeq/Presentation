b6d6fdd0a1a30f57c34e5107c959d0a4b610c0a2
Shafeeq <p180039@nu.edu.pk>
2019-09-10 22:34:58 +0500

    Unit Testing

---
+++ abs.py
@@ -0,0 +1,5 @@
+def abs(x):
+    if x < -1:
+        return -x
+    else:
+        return x
---
+++ test_abs.py
@@ -0,0 +1,10 @@
+from abs import abs
+
+def test_abs_1():
+    assert abs(-2) == 2
+
+def test_abs_2():
+    assert abs(2) == 2
+
+#def test_abs_3():
+#    assert abs(-1) == 1
